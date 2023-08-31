a414c8fe9dec5d7c3cea9fb4ebbd26cb7cd9e99f
Danil Stasenko <121118618+DanilStasenko@users.noreply.github.com>
2023-08-31 13:46:16 +0400

    Delete dist directory

--- dist/MyButton/MyButton.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-import './MyButton.css';
-export interface MyButtonProps {
-    children: any;
-    color: string;
-    big?: boolean;
-}
-declare const MyButton: FC<MyButtonProps>;
-export default MyButton;
